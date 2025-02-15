2362c79edd13a5b14aa03bc0c5050c622bfedd9c
MaxAlmeida <maxwell@tecnologiamx.com.br>
2025-02-15 19:06:30 -0300

    added `home`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,27 @@
-"use client";
+import Header from "@/components/Header";
+import Image from "next/image";
 
-import { useState, useEffect } from "react";
+import Hero from "../../public/assets/hero.png";
 
 export default function Home() {
-  const [inputName, setInputName] = useState("");
-  const [tasks, setTasks] = useState(["teste 1", "teste 2"]);
-
-  /// useEffect ao montar o componente
-  useEffect(() => {
-    const taskStorage = localStorage.getItem("@tasks");
-
-    if (taskStorage) {
-      setTasks(JSON.parse(taskStorage));
-    }
-  }, []);
-
-  /// useEffect ao manipular tasks
-  useEffect(() => {
-    localStorage.setItem("@tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
-  function handleSubmit(e: { preventDefault: () => void }) {
-    e.preventDefault();
-
-    if (inputName != "") {
-      setTasks([...tasks, inputName]);
-      setInputName("");
-    }
-  }
-
   return (
-    <div>
-      <h1>Bem vindo ao Projeto Tasks</h1>
-
-      <form onSubmit={handleSubmit}>
-        <label>Tarefa:</label> <br />
-        <input
-          type="text"
-          placeholder="Digite uma Tarefa"
-          value={inputName}
-          onChange={(event) => setInputName(event.target.value)}
-        />
-        <br />
-        <br />
-        <button type="submit">Adicionar Tarefa</button>
-      </form>
-
-      <br />
-      <br />
-
-      <ul>
-        {tasks.map((task) => (
-          <li key={task}>{task}</li>
-        ))}
-      </ul>
-    </div>
+    <main className="flex flex-col h-screen">
+      <Header />
+      <div className="bg-gray-950 flex flex-col justify-center items-center p-20 text-white h-screen">
+        <Image src={Hero} alt="Imagem Home" className="max-h-96" />
+        <h1 className="m-10 text-center text-2xl leading-relaxed">
+          Sistema feito para voce organizar <br />
+          seus estudos e tarefas
+        </h1>
+        <div className="flex flex-row space space-x-4">
+          <section className="secction">
+            <span>+ 12 posts</span>
+          </section>
+          <section className="secction">
+            <span>+ 90 comentarios</span>
+          </section>
+        </div>
+      </div>
+    </main>
   );
 }
