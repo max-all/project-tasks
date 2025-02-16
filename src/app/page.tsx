2b9ce7582375b3e6c58d3a87e90b1519d9fe4053
MaxAlmeida <maxwell@tecnologiamx.com.br>
2025-02-16 19:55:16 -0300

    added `Style` in `Home`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
       <Header />
       <div className="bg-gray-950 flex flex-col justify-center items-center p-20 text-white h-screen">
         <Image src={Hero} alt="Imagem Home" className="max-h-96" />
-        <h1 className="m-10 text-center text-2xl leading-relaxed">
+        <h1 className="m-10 text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl leading-relaxed">
           Sistema feito para voce organizar <br />
           seus estudos e tarefas
         </h1>
-        <div className="flex flex-row space space-x-4">
+        <div className="flex flex-col space space-x-0 sm:space-x-4 space-y-4 sm:space-y-0 sm:flex-row">
           <section className="secction">
             <span>+ 12 posts</span>
           </section>
