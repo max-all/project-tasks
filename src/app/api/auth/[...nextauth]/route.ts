588f681573ec8b97e0e5960ef8e5d9b5a574c110
MaxAlmeida <maxwell@tecnologiamx.com.br>
2025-02-17 17:38:30 -0300

    added NextApiHandler

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,16 @@
+import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
 
-export const authOptions = {
-  providers: [
-    Github({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-  ],
-  secret: process.env.JWT_SECRET as string,
-};
+const authHandler: NextApiHandler = (req, res) =>
+  NextAuth(req, res, {
+    providers: [
+      Github({
+        clientId: process.env.GITHUB_ID as string,
+        clientSecret: process.env.GITHUB_SECRET as string,
+      }),
+    ],
+    secret: process.env.JWT_SECRET,
+  });
 
-export default NextAuth(authOptions);
+export default authHandler;
