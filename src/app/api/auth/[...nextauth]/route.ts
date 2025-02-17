2103cb2ccaa5e3f950373c56e8e13c6082a6a7cb
MaxAlmeida <maxwell@tecnologiamx.com.br>
2025-02-17 19:04:43 -0300

    update for version 13+ next.js

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,16 @@
-import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
+import GithubProvider from "next-auth/providers/github";
 
-const authHandler: NextApiHandler = (req, res) =>
-  NextAuth(req, res, {
-    providers: [
-      Github({
-        clientId: process.env.GITHUB_ID as string,
-        clientSecret: process.env.GITHUB_SECRET as string,
-      }),
-    ],
-    secret: process.env.JWT_SECRET,
-  });
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  secret: process.env.JWT_SECRET,
+};
 
-export default authHandler;
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
