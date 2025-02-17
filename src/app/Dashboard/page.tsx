9af2543dfb6a65f7e169ce56e468b147bcc63813
MaxAlmeida <maxwell@tecnologiamx.com.br>
2025-02-17 15:40:20 -0300

    added `Dashboard page` for test

---
+++ src/app/Dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <main>
+        <h1>Teste</h1>
+      </main>
+    </div>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,16 @@ export default function Header() {
   return (
     <header className="flex justify-center px-10 py-4 bg-black">
       <section className="flex justify-between items-center min-w-full">
-        <nav>
+        <nav className="flex items-center space-x-2">
           <Link href="/">
             <h1 className="flex justify-center items-center text-white text-lg">
               Tarefas<span className="text-red-600 text-2xl ml-1">+</span>
             </h1>
+          </Link>
+          <Link href={"/Dashboard"}>
+            <button className="bg-white text-black px-4 rounded-sm text-sm">
+              Meu Painel
+            </button>
           </Link>
         </nav>
         <h2 className="flex justify-center items-center text-white w-28 border rounded-full">
