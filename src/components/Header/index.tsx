4c55707cf65c801f8da4baa137c58f797f7528e6
MaxAlmeida <maxwell@tecnologiamx.com.br>
2025-02-16 21:01:50 -0300

    added `Styles` in `Header`

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <div className="flex justify-between px-10 py-4">
-      <h1>Tarefas+</h1>
-      <h2>Ola Max</h2>
-    </div>
+    <header className="flex justify-center px-10 py-4 bg-black">
+      <section className="flex justify-between items-center min-w-full">
+        <nav>
+          <Link href="/">
+            <h1 className="flex justify-center items-center text-white text-lg">
+              Tarefas<span className="text-red-600 text-2xl ml-1">+</span>
+            </h1>
+          </Link>
+        </nav>
+        <h2 className="flex justify-center items-center text-white w-28 border rounded-full">
+          Ola Max
+        </h2>
+      </section>
+    </header>
   );
 }
