503037dc86aa20bac35dc1e06929a913ee67b92c
MaxAlmeida <maxwell@tecnologiamx.com.br>
2025-02-18 16:30:32 -0300

    test with hooks

---
+++ src/hooks/useProtectedRoute.ts
@@ -0,0 +1,15 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function useProtectedRoute() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "loading") return;
+    if (!session) router.push("/auth/signin");
+  }, [session, status, router]);
+
+  return { session, status };
+}
