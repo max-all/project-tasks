dca9554d14d5564de3ac1e574199c9ed675cbc4b
MaxAlmeida <maxwell@tecnologiamx.com.br>
2025-02-18 12:53:05 -0300

    update in style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,21 @@ import Hero from "../../public/assets/hero.png";
 
 export default function Home() {
   return (
-    <main className="bg-gray-950 flex flex-col justify-center items-center p-20 text-white h-screen">
-      <Image src={Hero} alt="Imagem Home" className="max-h-96" />
-      <h1 className="m-10 text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl leading-relaxed">
-        Sistema feito para voce organizar <br />
-        seus estudos e tarefas
-      </h1>
-      <div className="flex flex-col space space-x-0 sm:space-x-4 space-y-4 sm:space-y-0 sm:flex-row">
-        <section className="secction">
-          <span>+ 12 posts</span>
-        </section>
-        <section className="secction">
-          <span>+ 90 comentarios</span>
-        </section>
+    <main className="bg-gray-950 text-white">
+      <div className="flex flex-col justify-center items-center h-screen">
+        <Image src={Hero} alt="Imagem Home" className="max-h-96" />
+        <h1 className="m-10 text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl leading-relaxed">
+          Sistema feito para voce organizar <br />
+          seus estudos e tarefas
+        </h1>
+        <div className="flex flex-col space space-x-0 sm:space-x-4 space-y-4 sm:space-y-0 sm:flex-row">
+          <section className="secction">
+            <span>+ 12 posts</span>
+          </section>
+          <section className="secction">
+            <span>+ 90 comentarios</span>
+          </section>
+        </div>
       </div>
     </main>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,39 +7,41 @@ export default function Header() {
   const { data: session, status } = useSession();
 
   return (
-    <header className="flex justify-center px-10 py-4 bg-black">
-      <section className="flex justify-between items-center min-w-full">
-        <nav className="flex items-center space-x-2">
-          <Link href="/">
-            <h1 className="flex justify-center items-center text-white text-lg">
-              Tarefas<span className="text-red-600 text-2xl ml-1">+</span>
-            </h1>
-          </Link>
-          <Link href={"/Dashboard"}>
-            <button className="bg-white text-black px-4 rounded-sm text-sm">
-              Meu Painel
-            </button>
-          </Link>
-        </nav>
+    <header className="bg-black">
+      <div className="flex justify-center px-6 py-4 max-w-screen-xl m-auto">
+        <section className="flex justify-between items-center min-w-full">
+          <nav className="flex items-center space-x-2">
+            <Link href="/">
+              <h1 className="flex justify-center items-center text-white text-lg">
+                Tarefas<span className="text-red-600 text-2xl ml-1">+</span>
+              </h1>
+            </Link>
+            <Link href={"/Dashboard"}>
+              <button className="bg-white text-black px-4 rounded-sm text-sm">
+                Meu Painel
+              </button>
+            </Link>
+          </nav>
 
-        {status === "loading" ? (
-          <></>
-        ) : session ? (
-          <button
-            className="bg-white text-black p-sm min-w-24 max-w-sm rounded-full"
-            onClick={() => signOut()}
-          >
-            Olá {session.user?.name}
-          </button>
-        ) : (
-          <button
-            className="bg-white text-black p-sm min-w-24 max-w-sm rounded-full"
-            onClick={() => signIn("github")}
-          >
-            Acessar
-          </button>
-        )}
-      </section>
+          {status === "loading" ? (
+            <></>
+          ) : session ? (
+            <button
+              className="bg-white text-black p-sm min-w-24 max-w-sm rounded-full"
+              onClick={() => signOut()}
+            >
+              Olá {session.user?.name}
+            </button>
+          ) : (
+            <button
+              className="bg-white text-black p-sm min-w-24 max-w-sm rounded-full"
+              onClick={() => signIn("github")}
+            >
+              Acessar
+            </button>
+          )}
+        </section>
+      </div>
     </header>
   );
 }
