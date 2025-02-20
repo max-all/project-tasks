14d15914d4d23a8e96cce6de94000d8eb9aa58aa
MaxAlmeida <maxwell@tecnologiamx.com.br>
2025-02-20 12:38:20 -0300

    added `firebase` in dashboard for `newTask` and `tasks`

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -5,20 +5,59 @@ import { useRouter } from "next/navigation";
 import { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import Image from "next/image";
 import TextArea from "@/components/TextArea";
+import { Tasks } from "@/types/Tasks";
+
+import { db } from "@/services/firebaseConnection";
+import {
+  addDoc,
+  collection,
+  query,
+  orderBy,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
 
 export default function Dashboard() {
-  const { status } = useSession();
+  const { status, data } = useSession();
   const router = useRouter();
 
   const [task, setTask] = useState("");
   const [taskPublic, setTaskPublic] = useState(false);
   const [buttonDisable, setButtonDisable] = useState(true);
+  const [tasks, setTasks] = useState<Tasks[]>([]);
 
   useEffect(() => {
     if (status === "unauthenticated") {
       router.push("/");
+    } else {
+      async function loadTasks() {
+        const taskRef = collection(db, "tasks");
+        const q = query(
+          taskRef,
+          orderBy("createdAt", "desc"),
+          where("user", "==", data?.user?.name)
+        );
+
+        onSnapshot(q, (snapshot) => {
+          let list = [] as Tasks[];
+
+          snapshot.forEach((doc) => {
+            list.push({
+              id: doc.id,
+              task: doc.data().task,
+              taskPublic: doc.data().taskPublic,
+              createdAt: doc.data().createdAt,
+              user: doc.data().user,
+            });
+          });
+
+          setTasks(list);
+        });
+      }
+
+      loadTasks();
     }
-  }, [status, router]);
+  }, [status, router, data?.user?.name]);
 
   useEffect(() => {
     if (task !== "" || null) {
@@ -28,13 +67,21 @@ export default function Dashboard() {
     }
   }, [task]);
 
-  console.log(task);
-
-  function handleTaskRegister(event: FormEvent) {
+  async function handleTaskRegister(event: FormEvent) {
     event.preventDefault();
 
-    if (task != "") {
-      alert("Teste");
+    try {
+      await addDoc(collection(db, "tasks"), {
+        task: task,
+        taskPublic: taskPublic,
+        createdAt: new Date(),
+        user: data?.user?.name,
+      });
+
+      setTask("");
+      setTaskPublic(false);
+    } catch (error) {
+      console.log(error);
     }
   }
 
@@ -45,6 +92,7 @@ export default function Dashboard() {
   if (status === "authenticated") {
     return (
       <main className="min-h-screen flex flex-col">
+        {/* Section para Adicionar nova tarefa */}
         <section className="bg-gray-950 text-white p-10">
           <form
             onSubmit={handleTaskRegister}
@@ -78,10 +126,12 @@ export default function Dashboard() {
             </button>
           </form>
         </section>
+        {/* Section para Listagem de tarefas */}
         <section className="flex-grow">
           <div className="flex - flex-col justify-center items-center max-w-screen-xl mx-auto p-4">
             <h1 className="text-3xl m-2">Minhas Tarefas</h1>
             <article className="flex flex-col border border-neutral-400 rounded-sm w-full my-4">
+              {/* Section caso a tarefa seja publicas */}
               <section className="flex space-x-4 mx-4 my-2">
                 <span className="bg-blue-700 text-white px-2 rounded-sm cursor-pointer">
                   Publica
@@ -93,6 +143,7 @@ export default function Dashboard() {
                   alt="icon share"
                 />
               </section>
+              {/* Section referente a tarefa */}
               <section className="flex space-x-4 mx-4 my-2">
                 <p className="w-11/12 text-lg">Minha primeira Tarefa</p>
                 <Image
