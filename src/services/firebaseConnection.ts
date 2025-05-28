932fac7c71c419825064d728a3b0ac2b3819c07d
MaxAlmeida <maxwell@tecnologiamx.com.br>
2025-05-28 11:02:04 -0300

    update in projetct for tasks

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -1,13 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBUpJe-VwQdRlDPaGI6ip0u6LZ4qCE-6LA",
   authDomain: "project-tasks-9c324.firebaseapp.com",
@@ -18,9 +11,7 @@ const firebaseConfig = {
   measurementId: "G-R5QBES1GRC",
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(firebaseApp);
 
 const db = getFirestore(firebaseApp);
 
